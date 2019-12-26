0f64d7245110109022a17f5de1c47d8cf5e37dc6
ohjonah <ohjonah@gmail.com>
2019-12-26 11:17:19 -0800

    Adds work

---
+++ src/components/Account/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+import { withAuthorization, AuthUserContext } from '../Session';
+
+const AccountPage = () => (
+    <AuthUserContext.Consumer>
+        {authUser => (
+            <div>
+                <h1>Account Page for: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        )}
+    </AuthUserContext.Consumer>
+)
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,43 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        console.log('app:', app);
+
+        this.auth = app.auth();
+        this.db = app.database();
+    }
+
+    // *** Auth API ***
+
+    doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+    // *** User API ***
+
+    user = uid => this.db.ref(`users/${uid}`);
+
+    users = () => this.db.ref('users');
+}
+
+export default Firebase;
